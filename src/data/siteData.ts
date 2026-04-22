fb1b0910006d31c9aa5fb0cf95f2f335c060b0a3
Yogesh Avadhut <you@example.com>
2026-04-23 01:32:30 +0530

    map fix

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -35,8 +35,9 @@ export const business = {
   defaultOrderMessage: "Hello Renuka's Kitchen, I want to order a cake 🎂",
   address:
     "Near Hiremat Hospital, Opposite Shrimam Bahusaheb Zadbuke Complex, Agalgaon Road, Barshi",
+    
   mapsLink: "https://www.google.com/maps/place/New+gurukrupa+Kirana+stores/@18.2459247,75.6943167,17z/data=!3m1!4b1!4m6!3m5!1s0x3bc455fe296d3573:0xdb8a5e0ca586e266!8m2!3d18.2459196!4d75.6968916!16s%2Fg%2F11k9sb28zf?hl=en-GB&entry=ttu&g_ep=EgoyMDI2MDQyMC4wIKXMDSoASAFQAw%3D%3D",
-  embedMapUrl: "https://www.google.com/maps/place/New+gurukrupa+Kirana+stores/@18.2459247,75.6943167,17z/data=!3m1!4b1!4m6!3m5!1s0x3bc455fe296d3573:0xdb8a5e0ca586e266!8m2!3d18.2459196!4d75.6968916!16s%2Fg%2F11k9sb28zf?hl=en-GB&entry=ttu&g_ep=EgoyMDI2MDQyMC4wIKXMDSoASAFQAw%3D%3D"
+  embedMapUrl: "https://www.google.com/maps?q=18.2459196,75.6968916&z=17&output=embed"
 };
 
 export const navLinks: NavLink[] = [
